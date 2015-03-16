a0e8e49d94f1d947e71009ec84c786b671c159e7
Gearoid Sugrue <Gearoid.Sugrue@outlook.com>
2015-03-16 22:57:07 +0000

    Removed some unused code

--- app/src/main/java/com/example/gearoid/testchatapp/GameSetupActivity.java
+++ app/src/main/java/com/example/gearoid/testchatapp/GameSetupActivity.java
@@ -1,6 +1,5 @@
 package com.example.gearoid.testchatapp;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,14 +7,38 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.gearoid.testchatapp.character.ICharacter;
+
+import java.util.ArrayList;
+
 
 public class GameSetupActivity extends ActionBarActivity {
+
+    Board currentBoard;
+    int playerCount, evilCount, goodCount;
+    ArrayList<ICharacter> allCharacters;
+
+    public enum Board {
+        FIVE, SIX, SEVEN, EIGHT, NINE, TEN;
+    }
+
+    public enum Quest {
+        FIRST(1), SECOND(2), THIRD(3), FOURTH(4), FIFTH(5) ;
+        private int value;
+
+        private Quest(int value) {
+            this.value = value;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
         initializeButtons();
+
+        //setup + calculate...
+
     }
 
 
@@ -60,6 +83,48 @@ public class GameSetupActivity extends ActionBarActivity {
         //intent.putExtra("USER_LIST", userList);
         //startActivity(intent);
 
+    }
+    public Board calculateBoard(int numberOfPlayers){
+        switch (numberOfPlayers) {
+            case 5: return Board.FIVE;
+            case 6: return Board.SIX;
+            case 7: return Board.SEVEN;
+            case 8: return Board.EIGHT;
+            case 9: return Board.NINE;
+            case 10: return Board.TEN;
+        }
+        return Board.FIVE;
+    }
+
+    public int calculateNumberOfEvilPlayers(Board board){
+
+        switch (board) {
+            case FIVE:
+            case SIX: return 2;
+            case SEVEN:
+            case EIGHT:
+            case NINE: return 3;
+            case TEN: return 4;
+        }
+        return 2;
+    }
+
+    public static int[] getBoardConfiguration(Board board){
+
+        int[] arr;
+        switch (board) {
+            case FIVE: return arr = new int[]{2,3,2,3,3};
+            case SIX: return arr = new int[]{2,3,4,3,4};
+            case SEVEN: return arr = new int[]{2,3,3,4,4};
+            case EIGHT:
+            case NINE:
+            case TEN: return arr = new int[]{3,4,4,5,5};
+        }
+        return arr = new int[]{0,0,0,0,0};
+    }
+
+    public static int calculatePlayersOnQuest(Board board, Quest questNumber){//Move this???
+        return getBoardConfiguration(board)[questNumber.value];
     }
 
 }
